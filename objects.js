0c5ef2f9d932586b67e218459f23c5fd0473f81c
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-17 12:44:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,14 @@
 var playlist = {"Arijit singh" : "Khamosiyan",
                   "Baadshah" : "Mercy",
                   "Honey Singh" : "Yo Yo "
-  
+
             }
 
 
 var updatePlaylist = (obj,name,song) =>{
-  obj[name] = song
-  return obj 
-  
+  obj.name = song
+  return obj
+
 }
 
 var removeFromPlaylist = (obj,name) =>{
